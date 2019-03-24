a644043d4d64806138691cf75bf7b74eff240f63
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-24 20:57:29 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -25,4 +25,27 @@ async def announce(ctx, channel: discord.Channel=None, *, msg: str):
     await bot.send_message(channel, embed=embed)
     await bot.delete_message(ctx.message)
     
+@client.command(pass_context =True)
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(Colour = discord.Colour.orange())
+    embed.set_author(name = 'Help Commands')
+    embed.add_field(name ='hs!say', value ='Returns what the user says.', inline=False)
+    embed.add_field(name ='hs!announce', value ='Announce something on an specified channel.', inline=False)
+    embed.add_field(name ='hs!serverinfo', value ='Gives the server information on the selected user,so you must do hs!serverinfo and mention the user!', inline=False)
+
+    await client.send_message(author, embed=embed)
+    await client.say('📬 Check your DMs!')
+    
+@client.command(pass_context=True)
+async def serverinfo(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await client.say(embed=embed)
+    
 bot.run('NTU5MDk4Mjk0ODA3MTY2OTg4.D3gccQ.mgq1wIkY2_xhdgaXJnq3sRHdwsc')
