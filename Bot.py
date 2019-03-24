21b789a463520a763392f7d34ccbf8586e610155
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-24 19:58:02 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,23 +1,21 @@
-import asyncio
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
 
-
-Mike = Bot('hs!')
-
-@Mike.event
+bot=commands.Bot(command_prefix='hs!')
+@bot.event
 async def on_ready():
-    print ("Starting up")
-    print ("My username is " + Mike.user.name + " and i am running with the ID: " + Mike.user.id)
-    await Mike.change_presence(game=discord.Game(name="the community!", type=3))
-    print ("Started")
-    
-Mike.remove_command('help')
+    print('Logged in as '+bot.user.name+' (ID:'+bot.user.id+') | Connected to '+str(len(bot.servers))+' servers | Connected to '+str(len(set(bot.get_all_members())))+' users')
+    print('--------')
+    await bot.change_presence(game=discord.Game(name="the community! 👀", type=3))
 
-@Mike.command(pass_context = True)
+bot.remove_command('help')
+
+@bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
 async def say(ctx, *args):
     mesg = ' '.join(args)
-    await Mike.delete_message(ctx.message)
-    return await Mike.say(mesg)
+    await bot.delete_message(ctx.message)
+    return await bot.say(mesg)
    
-Mike.run('NTU5MDk4Mjk0ODA3MTY2OTg4.D3gccQ.mgq1wIkY2_xhdgaXJnq3sRHdwsc')
+bot.run('NTU5MDk4Mjk0ODA3MTY2OTg4.D3gccQ.mgq1wIkY2_xhdgaXJnq3sRHdwsc')
